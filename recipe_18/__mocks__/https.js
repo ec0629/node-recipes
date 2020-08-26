808b9e1cf433d557c030c1916ad8802db9e39789
Jeff Simonitto <jsimonitto@gmail.com>
2020-08-25 21:14:13 -0400

    functionality to mock out second call to request

--- recipe_18/__mocks__/https.js
+++ recipe_18/__mocks__/https.js
@@ -1,14 +1,25 @@
 const { PassThrough } = require('stream');
-const gitJson = require('../mockGitHubUserData.json');
+const userJson = require('../mockGitHubUserData.json');
+const repoJson = require('../mockGitHubRepoData.json');
 
 const https = jest.createMockFromModule('https');
 
+const jsonResponses = [userJson, repoJson];
+let idx = 0;
+
+function getJson() {
+  let resp = jsonResponses[idx];
+  if (idx !== jsonResponses.length - 1) {
+    idx += 1;
+  }
+  return resp;
+}
+
 function request(options, callback) {
-  // const gitJson = { login: 'jonathanfmills' };
-  const gitResponse = new PassThrough();
-  gitResponse.write(JSON.stringify(gitJson));
-  callback(gitResponse);
-  return gitResponse;
+  const responseStream = new PassThrough();
+  responseStream.write(JSON.stringify(getJson()));
+  callback(responseStream);
+  return responseStream;
 }
 
 https.request = request;
